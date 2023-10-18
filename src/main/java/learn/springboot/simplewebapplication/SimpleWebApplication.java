08d0f06ac149bd3370d46535052d64f1ea1b789d
alkasemm <alkasemm@encowayhb.lokal>
2023-10-18 08:51:45 +0200

    first comment

---
+++ src/main/java/learn/springboot/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,16 @@
+package learn.springboot.simplewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+		
+	}
+
+
+
+}
---
+++ src/test/java/learn/springboot/simplewebapplication/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springboot.simplewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

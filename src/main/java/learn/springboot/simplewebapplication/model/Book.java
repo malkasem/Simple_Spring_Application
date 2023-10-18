0c7159c04a4ce68647f2e31ef1c19050f14ca803
alkasemm <alkasemm@encowayhb.lokal>
2023-10-18 10:03:20 +0200

    Entity in Hibernate Relrelationship

--- src/main/java/learn/springboot/simplewebapplication/model/Book.java
+++ src/main/java/learn/springboot/simplewebapplication/model/Book.java
@@ -1,2 +1,57 @@
-package learn.springboot.simplewebapplication.model;public class Book {
+package learn.springboot.simplewebapplication.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> author;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Set<Author> author) {
+        this.author = author;
+    }
 }
